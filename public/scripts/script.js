743bdf4920bccff7ecdb823c4f773746c112cf3e
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-04-16 21:31:48 -0400

    edited colors, added loading wheel, about section, and link previews

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,21 @@
 $(window).on('load', function() {
+  
     $("#demo").empty();
       console.log('scraping')
     $.get("/scrape", function(response) {
         var info = response;
         $("#demo").load("/scrape .news-block h3, .news-block .wp-post-image, .news-block .news-excerpt");
+        var y = document.getElementById("info");
+        y.style.display = "none";
+        var x = document.getElementById("loader");
+        x.style.display = "none";
+        
+    });
+
+    $("#demo").on("mouseover", function() {
+      $('a').miniPreview({ prefetch: 'none' });
     })
+
   });
+
+  
